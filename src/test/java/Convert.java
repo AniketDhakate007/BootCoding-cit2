a6d398055107b9be854a7f249424f6618fbaec18
AniketDhakate007 <adhakate344@gmail.com>
2023-08-17 15:52:55 +0530

    basic Programs

---
+++ src/test/java/Convert.java
@@ -0,0 +1,10 @@
+public class Convert {
+    public static void main(String[] args) {
+        int hour = 1;
+        int minutes = 5;
+        int seconds = minutes*60;
+        int hourSeconds = hour*60*60;
+        System.out.println("hour to Seconds: "+hourSeconds);
+        System.out.println("minutes to Seconds: "+seconds);
+    }
+}
